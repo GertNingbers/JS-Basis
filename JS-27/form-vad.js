30266cdd8c1585c435c24d9bc805f2cfffb5ffbf
GertNingbers <97081115@st.deltion.nl>
2024-11-11 10:36:17 +0100

    V3 form validatie

--- JS-27/form-vad.js
+++ JS-27/form-vad.js
@@ -1,80 +1,87 @@
+document.getElementById("verzend").addEventListener("click", check);
+
 const voornaam = document.getElementById("voornaam");
 const tv = document.getElementById("tv");
 const achternaam = document.getElementById("achternaam");
 const adres = document.getElementById("adres");
 const postcode = document.getElementById("postcode");
 const plaats = document.getElementById("plaats");
-
 const telefoon = document.getElementById("telefoon");
 const email = document.getElementById("email");
-const geslacht = document.getElementById("geslacht");
-const boyband = document.getElementById("boyband");
 const geboortedatum = document.getElementById("geboorte");
+const boyband = document.getElementById("boyband");
 
 let text = "";
 
-function check() {
+function check(event) {
+    event.preventDefault(); // Voorkomt dat pagina opnieuw wordt geladen.
+    text = "";
 
+    // Controleert op verplichte velden.
     if (!voornaam.value || !achternaam.value || !adres.value || !postcode.value || !plaats.value) {
-        text = "";
+        if (!voornaam.value) {
+            voornaam.style.backgroundColor = "red";
+            text += "Voornaam is verplicht<br>";
+        } else {
+            document.getElementById("voornaam").style.backgroundColor = "green";
+        }
 
-        switch (true) {
-            case (voornaam.value === ""): voornaam.style.backgroundColor = "red";
-                text += "Voornaam is verplicht" + "<br>";
-                break;
-            case (voornaam.value !== ""): voornaam.style.backgroundColor = "green";
-                break;
+        if (!achternaam.value) {
+            achternaam.style.backgroundColor = "red";
+            text += "Achternaam is verplicht<br>";
+        } else {
+            achternaam.style.backgroundColor = "green";
+        }
 
-            case (achternaam.value === ""): achternaam.style.backgroundColor = "red";
-                text += "Achternaam is verplicht" + "<br>";
-                break;
-            case (achternaam.value !== ""): achternaam.style.backgroundColor = "green";
-                break;
+        if (!adres.value) {
+            adres.style.backgroundColor = "red";
+            text += "Adres is verplicht<br>";
+        } else {
+            adres.style.backgroundColor = "green";
+        }
 
-            case (adres.value === ""): adres.style.backgroundColor = "red";
-                text += "Adres is verplicht" + "<br>";
-                break;
-            case (adres.value !== ""): adres.style.backgroundColor = "green";
-                break;
+        if (!postcode.value) {
+            postcode.style.backgroundColor = "red";
+            text += "Postcode is verplicht<br>";
+        } else {
+            postcode.style.backgroundColor = "green";
+        }
 
-            case (postcode.value === ""): postcode.style.backgroundColor = "red";
-                text += "Postcode is verplicht" + "<br>";
-                break;
-            case (postcode.value !== ""): postcode.style.backgroundColor = "green";
-                break;
-                
-            case (plaats.value === ""): plaats.style.backgroundColor = "red";
-                text += "Plaats is verplicht" + "<br>";
-                break;
-            case (plaats.value !== ""): plaats.style.backgroundColor = "green";
-                break;
-                default:;
+        if (!plaats.value) {
+            plaats.style.backgroundColor = "red";
+            text += "Plaats is verplicht<br>";
+        } else {
+            plaats.style.backgroundColor = "green";
         }
 
         document.getElementById("melding").innerHTML = text;
         document.getElementById("melding").style.color = "red";
     } else {
-        text = "";
-        text = "UW naam is: " + voornaam.value + " " + tv.value + " " + achternaam.value + "<br> Adres is: " + adres.value + "<br>Postcode: " + postcode.value + " En plaats: " + plaats.value + "<br>";
+        text = "Uw naam is: " + voornaam.value + " " + tv.value + " " + achternaam.value + "<br> Adres is: " + adres.value + "<br>Postcode: " + postcode.value + " " + plaats.value + "<br>";
 
         if (telefoon.value) {
             text += "Jouw telefoonnummer is: " + telefoon.value + ".<br>";
         }
+
         if (email.value) {
             text += "Jouw email is: " + email.value + ".<br>";
         }
-        if (boyband.value) {
-            text += "Jouw favoriete boyband is: " + boyband.value + ".<br>";
-        }
-        if (geslacht.value) {
-            text += "Jouw geslacht is: " + geslacht.value + ".<br>";
-        }
+
         if (geboortedatum.value) {
             text += "Jouw geboortedatum is: " + geboortedatum.value + ".<br>";
         }
 
+        if (boyband.value) {
+            text += "Jouw favoriete boyband is: " + boyband.value + ".<br>";
+        }
+
+        const geslacht = document.querySelector('input[name="geslacht"]:checked');
+        if (geslacht) {
+            text += "Jouw geslacht is: " + geslacht.value + ".<br>";
+        }
+
         document.getElementById("melding").innerHTML = text;
-
+        document.getElementById("melding").style.color = "black";
     }
-
 }
+
